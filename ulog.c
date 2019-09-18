c8356022548e23c9d5d1087417ac8a0238a70db5
TyK <tisyang@gmail.com>
2019-09-18 16:18:09 +0800

    init repo

---
+++ ulog.c
@@ -0,0 +1,108 @@
+#include "ulog.h"
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+
+// ulog default print function
+static int ulog_default_print(void* userdata, int tag, const char* line);
+
+
+// ulog global variables
+static print_func m_print = ulog_default_print;
+static void* m_userdata = NULL;
+static int   m_filter = ULOG_LV_ALL;
+
+// just print to stdout
+static int ulog_default_print(void* userdata, int tag, const char* line)
+{
+    printf("%s", line);
+}
+
+static const char* const TAG_NAMES[] = {
+    [ULOG_TAG_TRACE]     = "[TRACE]",
+    [ULOG_TAG_DEBUG]     = "[DEBUG]",
+    [ULOG_TAG_INFO]      = "[INFO ]",
+    [ULOG_TAG_WARN]      = "[WARN ]",
+    [ULOG_TAG_ERROR]     = "[ERROR]",
+};
+
+static const char* const TAG_COLORS[] = {
+    [ULOG_TAG_TRACE]     = "\x1b[94m",
+    [ULOG_TAG_DEBUG]     = "\x1b[36m",
+    [ULOG_TAG_INFO]      = "\x1b[32m",
+    [ULOG_TAG_WARN]      = "\x1b[33m",
+    [ULOG_TAG_ERROR]     = "\x1b[31m",
+};
+#define COLOR_ADDR  "\x1b[90m"
+#define COLOR_RESET "\x1b[0m"
+
+
+void ulog_log(int tag, const char *file, int lineno, const char *func, const char *format, ...)
+{
+    // ignore log tag not in filters
+    if (!(tag & m_filter)) {
+        return;
+    }
+
+    // get file name from file path
+    for (const char* p = file; *p; p++) {
+        if (*p == '/' || *p == '\\') {
+            file = p + 1;
+        }
+    }
+    // get timestamp
+    struct timespec now;
+    clock_gettime(CLOCK_REALTIME, &now);
+    struct tm *tminfo = localtime(&now.tv_sec);
+
+    int idx = 0;
+    char line[ULOG_LINE_MAXCHAR];
+#if ULOG_ENABLE_TIME
+# if ULOG_ENABLE_DATE
+    idx += strftime(line + idx, sizeof(line) - idx, "%F %T", tminfo);
+# else
+    idx += strftime(line + idx, sizeof(line) - idx, "%T", tminfo);
+# endif
+# if ULOG_ENABLE_MILLISECOND
+    idx += snprintf(line + idx, sizeof(line) - idx, ".%03d", now.tv_nsec / 1000000L);
+# endif
+#endif
+
+    idx += snprintf(line + idx, sizeof(line) - idx,
+#if ULOG_ENABLE_COLOR
+# if ULOG_ENABLE_ADDR
+                    " %s%s %s%s:%d%s: ",
+                    TAG_COLORS[tag], TAG_NAMES[tag],
+                    COLOR_ADDR, file, lineno, COLOR_RESET);
+# else
+                    " %s%s%s: ",
+                    TAG_COLORS[tag], TAG_NAMES[tag], COLOR_RESET);
+# endif
+#else
+# if ULOG_ENABLE_ADDR
+                    " %s %s:%d: ",
+                    TAG_NAMES[loglevel], file, lineno);
+# else
+                    " %s: ",
+                    TAG_NAMES[loglevel]);
+# endif
+#endif
+    va_list arglist;
+    va_start(arglist, format);
+    idx += vsnprintf(line + idx, sizeof(line) - idx, format, arglist);
+    va_end(arglist);
+    // add new line
+    idx += snprintf(line + idx, sizeof(line) - idx, "\n");
+    // output
+    m_print(m_userdata, tag, line);
+}
+
+
+void ulog_init(print_func func, void *userdata, int filter)
+{
+    m_print = func ? func : ulog_default_print;
+    m_userdata = userdata;
+    m_filter = filter;
+}
+
---
+++ ulog.h
@@ -0,0 +1,76 @@
+#ifndef ULOG_H
+#define ULOG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// configuration macros
+// max chars in one line, including terminal null char
+#define ULOG_LINE_MAXCHAR   512
+
+// enable color by default
+#ifndef ULOG_DISABLE_COLOR
+# define ULOG_ENABLE_COLOR 1
+#endif
+
+// enable log address like file:linenumber by default
+#ifndef ULOG_DISABLE_ADDR
+# define ULOG_ENABLE_ADDR 1
+#endif
+
+// enable timestamp by default
+#ifndef ULOG_DISABLE_TIME
+# define ULOG_ENABLE_TIME 1
+
+// enable millisecond in timestamp by default
+# ifndef ULOG_DISABLE_MILLISECOND
+#  define ULOG_ENABLE_MILLISECOND 1
+# endif
+
+// enable date in timestamp by default
+# ifndef ULOG_DISABLE_DATE
+#  define ULOG_ENABLE_DATE 1
+# endif
+
+#endif
+
+// log tags
+#define ULOG_TAG_TRACE   (1 << 0)
+#define ULOG_TAG_DEBUG   (1 << 1)
+#define ULOG_TAG_INFO    (1 << 2)
+#define ULOG_TAG_WARN    (1 << 3)
+#define ULOG_TAG_ERROR   (1 << 4)
+
+
+#define ULOG_LV_NONE    (0)
+#define ULOG_LV_ERROR   (ULOG_TAG_ERROR)
+#define ULOG_LV_WARN    (ULOG_TAG_WARN | ULOG_LV_ERROR)
+#define ULOG_LV_INFO    (ULOG_TAG_INFO | ULOG_LV_WARN)
+#define ULOG_LV_DEBUG   (ULOG_TAG_DEBUG | ULOG_LV_INFO)
+#define ULOG_LV_TRACE   (ULOG_TAG_TRACE | ULOG_LV_DEBUG)
+#define ULOG_LV_ALL     (ULOG_LV_TRACE)
+
+
+typedef int (*print_func)(void* userdata, int tag, const char* line);
+
+
+//
+void ulog_init(print_func func, void *userdata, int filter);
+
+
+//
+void ulog_log(int tag, const char *file, int lineno, const char *func, const char *format, ...);
+
+#define	LOG_TRACE(...)  ulog_log(ULOG_TAG_TRACE, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define	LOG_DEBUG(...)  ulog_log(ULOG_TAG_DEBUG, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define	LOG_INFO(...)   ulog_log(ULOG_TAG_INFO,  __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define	LOG_WARN(...)   ulog_log(ULOG_TAG_WARN,  __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define	LOG_ERROR(...)  ulog_log(ULOG_TAG_ERROR, __FILE__, __LINE__, __func__, __VA_ARGS__)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ULOG_H */
+
